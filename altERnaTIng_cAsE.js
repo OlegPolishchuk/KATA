a936aa7d1ddfec9bdd4044f0c97c41aa1b4e3c19
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-02-07 19:57:18 +0300

    refactor: change code

--- altERnaTIng_cAsE.js
+++ altERnaTIng_cAsE.js
@@ -3,24 +3,29 @@
 // see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
 //"hello world".toAlternatingCase() === "HELLO WORLD"
 
+// String.prototype.toAlternatingCase = function () {
+//
+//     let lowReg = /[a-z]/g;
+//     let upReg = /[A-Z]/g;
+//     let result = '';
+//
+//     for (let i = 0; i < this.length; i++){
+//         // console.log(result[i].search(lowReg));
+//         if (this[i].search(lowReg) !== -1){
+//             result += '' + this[i].toUpperCase()
+//         } else if (this[i].search(upReg) !== -1) {
+//             result += '' + this[i].toLowerCase()
+//         } else if ( this[i] === ' '){
+//             result += ' '
+//         } else {
+//             result += '' + this[i]
+//         }
+//     }
+//     return result
+//
+// };
+
+// короткое решение
 String.prototype.toAlternatingCase = function () {
-
-    let lowReg = /[a-z]/g;
-    let upReg = /[A-Z]/g;
-    let result = '';
-
-    for (let i = 0; i < this.length; i++){
-        // console.log(result[i].search(lowReg));
-        if (this[i].search(lowReg) !== -1){
-            result += '' + this[i].toUpperCase()
-        } else if (this[i].search(upReg) !== -1) {
-            result += '' + this[i].toLowerCase()
-        } else if ( this[i] === ' '){
-            result += ' '
-        } else {
-            result += '' + this[i]
-        }
-    }
-    return result
-
+    return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
 };
