0cba6fdfb1df15cd56d14e4d2b402eaef6e49692
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-02-17 18:54:13 +0300

    end task

--- Find_the_Middle_of_the_Product.js
+++ Find_the_Middle_of_the_Product.js
@@ -11,3 +11,24 @@
 //
 // NOTE: Remove leading zeros if product is even and the first digit of the two is a zero. Example 2016 -> 1
 //
+function findMiddle(str){
+    let regExp = /\d/g;
+
+    if (typeof str !== "string" || str.match(regExp) === null) return -1;
+
+    let multiply = str.match(regExp).reduce((a,b)=>a*b).toString().split('');
+    let result;
+
+
+    if (multiply.length % 2 !== 0){
+        result = multiply[Math.floor(multiply.length/2)]
+    } else {
+        result = `${multiply[Math.floor(multiply.length/2 - 1)]}`+ `${multiply[Math.floor(multiply.length/2)]}`
+    }
+
+    return parseInt(result);
+}
+
+console.log(findMiddle('s7d8jd9')); // 0 7*8*9 = 504 => по центру 0
+console.log(findMiddle('58jd9fgh/fgh6s.,sdf'));
+console.log(findMiddle(546));
